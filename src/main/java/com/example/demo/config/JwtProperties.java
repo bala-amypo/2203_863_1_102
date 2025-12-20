d026b1a43a735563b7cbacb9ae899697f1ac527f
bala-amypo <bala@amypo.in>
2025-12-20 13:41:54 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtProperties.java
+++ src/main/java/com/example/demo/config/JwtProperties.java
@@ -1,13 +1,13 @@
 package com.example.demo.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "jwt")
+@Data
 public class JwtProperties {
-    private String secret;
-    private long expiration;
-
-    // getters and setters
+    private String secret = "mySecretKey";
+    private Long expirationMs = 86400000L; // 24 hours
 }
