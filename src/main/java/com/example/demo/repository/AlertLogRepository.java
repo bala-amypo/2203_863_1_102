abc641b9bae0e052f56dec325e441186b6dc81e5
bala-amypo <bala@amypo.in>
2025-12-20 15:12:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertLogRepository.java
+++ src/main/java/com/example/demo/repository/AlertLogRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.AlertLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
 public interface AlertLogRepository extends JpaRepository<AlertLog, Long> {
     List<AlertLog> findByWarrantyId(Long warrantyId);
