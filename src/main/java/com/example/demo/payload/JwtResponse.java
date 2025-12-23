578933e4c2e676a60473e049866d041102870c66
bala-amypo <bala@amypo.in>
2025-12-23 09:52:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/payload/JwtResponse.java
@@ -0,0 +1,3 @@
+package com.example.demo.payload;
+
+public record JwtResponse(String token) {}
---
+++ src/main/java/com/example/demo/payload/LoginRequest.java
@@ -0,0 +1,3 @@
+package com.example.demo.payload;
+
+public record LoginRequest(String email, String password) {}
