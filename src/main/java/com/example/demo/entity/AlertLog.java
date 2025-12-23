10e3d9996a7ec4757b50ac437433eb08480a2bcf
bala-amypo <bala@amypo.in>
2025-12-23 05:55:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertLog.java
+++ src/main/java/com/example/demo/entity/AlertLog.java
@@ -15,7 +15,6 @@ public class AlertLog {
     private Long warrantyId;
     private String message;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
