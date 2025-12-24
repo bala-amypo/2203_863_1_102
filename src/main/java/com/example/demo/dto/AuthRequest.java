047683c0549a3d3a6adb78140dd1c71649b83d47
bala-amypo <bala@amypo.in>
2025-12-24 06:40:26 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,10 +1,10 @@
-package com.example.demo.dto;
+package com.example.demo.controller;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
 public class AuthRequest {
     private String email;
     private String password;
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,12 +1,13 @@
-package com.example.demo.dto;
+package com.example.demo.controller;
 
-import lombok.*;
-
-@Getter @Setter
-@AllArgsConstructor
 public class AuthResponse {
     private String token;
-    private Long userId;
     private String email;
     private String role;
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
