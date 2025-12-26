ca0ce278bc75e7221e0524c583261c62f76ffb72
bala-amypo <bala@amypo.in>
2025-12-26 08:49:32 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,38 +1,33 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9094.32procr.amypo.ai/")
-                ));
-        }
         SecurityScheme bearerAuth = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://9094.32procr.amypo.ai/")
+                ))
                 .components(
                         new Components().addSecuritySchemes("bearerAuth", bearerAuth)
                 )
                 .addSecurityItem(
                         new SecurityRequirement().addList("bearerAuth")
                 );
+    }
 }
