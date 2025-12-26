ac1fc07a08eee6608ad5840533ec3adf1532353c
bala-amypo <bala@amypo.in>
2025-12-26 09:46:41 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtProperties.java
+++ src/main/java/com/example/demo/config/JwtProperties.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class JwtProperties {
-    private String secret = "myVeryLongSecretKeyThatIsAtLeast32CharactersLongForSecurity";
+    private String secret = "myCustomSecretKeyForJWTThatIsAtLeast32CharactersLong";
     private Long expirationMs = 3600000L;
 }
