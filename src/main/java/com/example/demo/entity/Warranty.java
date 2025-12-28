7325913e96a15aaa3d8c231fcc704c41675530a7
bala-amypo <bala@amypo.in>
2025-12-28 19:11:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -1,23 +1,30 @@
 package com.example.demo.entity;
 
-import jakarta.validation.constraints.Email;
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "users")
+@Table(name = "warranties")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+
+public class Warranty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    private String name;
-    @Email(message = "Please provide a valid email address")
-    private String email;
-    private String password;
-    private String role;
+    private LocalDate purchaseDate;
+    private LocalDate expiryDate;
+    private String serialNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
